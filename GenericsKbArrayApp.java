50317e7d70c5c88bbbc83b6e83e24b941b96089f
kamogelo <mphkam016@myuct.ac.za>
2024-02-29 22:37:03 +0200

    first ten lines of the code

---
+++ GenericsKbArrayApp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class test{
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Choose an action from the menu:\n1. Load a knowledge base from a file\n2. Add a new statement to the knowledge base\n3. Search for an item in the knowledge base by term\n4. Search for an item in the knowledge base by term and sentence\n5. Quit");
+        System.out.print("Enter your choice: ");
+        int option = keyboard.nextInt();
+
+        while(option != 5){
+           
+            
+        }
